685af496ad7f143aaced3e310e0e77ac787887ba
makruff <161299631+makruff@users.noreply.github.com>
2024-03-06 22:40:58 +0700

    Update test_string

--- test_string.py
+++ test_string.py
@@ -3,10 +3,12 @@ from main import get_ssl_expiry_date
 
 def test_ssl_expiry_date_format():
   hostname = "sib.seal.or.id"
-  expiry_date= get_ssl_expiry_date(hostname)
-
+  expiry_date = get_ssl_expiry_date(hostname)
+  
+  # Check if expiry date is in the correct format (YYYY-MM_DD HH:MM:SS)
   assert re.match(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}", str(expiry_date) is not None
 
 def test_hostname_format():
-  hostname="sib.seal.or.id"
+  # Check if hostname is in the correct format
+  hostname = "sib.seal.or.id"
   assert re.match(r"^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$", hostname) is not None
